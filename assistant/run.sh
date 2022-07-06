c9a617163311199315bebdb6d529920a2bfaf48b
jmk <jminkim4210@gmail.com>
2022-07-07 00:33:38 +0900

    gpio 추가

--- assistant/run.sh
+++ assistant/run.sh
@@ -3,6 +3,7 @@ while [ 1 ]
    do
         pid=`ps -ef | grep "calltotalk.py" | grep -v 'grep' | awk '{print $2}'`
         if [ -z $pid ]; then
+        amixer -c Device cset numid=3 14
         ~/venv/bin/python calltotalk.py &
         fi
    done
